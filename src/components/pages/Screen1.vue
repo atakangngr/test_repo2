b1af5ff6c4f0e323e6bd5ce169ea1d89e80e9f61
NUCAL <yagmur.sahin@nucal.com>
2022-09-28 13:28:43 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,76 @@
+<template>
+  <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
+    v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="371"
+    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
+    <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
+      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
+      id="860d7118-3592-4688-af9e-8f11c33d3779" v-bind:col="0" v-bind:row="0" v-bind:colSpan="1" v-bind:rowSpan="1"
+      v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
+      v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
+      slot="r0-c0" area="r0-c0">
+      <WButtonAvatar src="" icon="" size="" text="" v-bind:badge="false" v-bind:button="true" v-bind:square="false"
+        v-bind:rounded="false" variant="secondary" v-bind:badgeTop="false" v-bind:disabled="false"
+        v-bind:badgeLeft="false" badgeText="" buttonText="Button Text Avatar" buttonType="button"
+        v-bind:aspectRatio="true" badgeOffset="" dataLoading="static" badgeVariant="primary" v-bind:hasDesignMode="true"
+        ncTooltipText=""
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModelForDynamicType'},'propName':'src','displayName':'Value'}]}"
+        dataModelForDynamicType="string" position="absolute" top="30.270881198934763%" left="26.588957055214724%"
+        id="ae689714-de08-4979-ba59-cc0eda0f33dc"
+        v-bind:style="{'width':'','height':'','maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
+      <TwAvatar imgWidth="tw-w-9" linkHref="#" imgHeight="tw-h-9" imgRadius="tw-rounded-full"
+        alignItems="tw-items-center" headerText="Tom Cook" headerColor="tw-text-gray-700"
+        imageSource="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+        textDisplay="tw-sr-only" ncTooltipText="" paragraphText="View profile" headerFontSize="tw-text-sm"
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'imageSource','displayName':'Value'}]}"
+        paragraphColor="tw-text-gray-500" headerFontFamily="tw-font-sans" headerFontWeight="tw-font-medium"
+        headerHoverColor="tw-ring-2" notificationRing="tw-ring-2" notificationColor="tw-bg-gray-300"
+        notificationWidth="tw-w-1.5" paragraphFontSize="tw-text-xs" notificationHeight="tw-h-1.5"
+        notificationRadius="tw-rounded-3xl" notificationDisplay="tw-sr-only" paragraphFontFamily="tw-font-sans"
+        paragraphFontWeight="tw-font-medium" paragraphHoverColor="tw-text-gray-700"
+        notificationPosition="tw-top-0 tw-right-0" notificationRingColor="tw-ring-white" position="absolute" top="0%"
+        left="56.058282208588956%" v-bind:snapX="false" v-bind:snapY="false" id="077ab781-f20b-4dee-9557-38870e04a7b5"
+        v-bind:style="{'width':'371px','height':'298px','fontSize':'9.982px','maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','backgroundColor':'','zIndex':3}"/>
+      <WInfoBadge href="" color="#FFFFFF" v-bind:exact="false" v-bind:active="false" v-bind:append="false"
+        caption="Info" variant="info" v-bind:disabled="false" linkTarget="_self" tagOfBadge="span"
+        v-bind:pillOfBadge="false" ncTypography="body1" ncTooltipText=""
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'caption','displayName':'Caption'}]}"
+        toForRouterLink="" ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#ff0000"
+        ncDisableFontColor="#4285f4" whichHeadTagForSize="h5" position="absolute" top="25.54782665444358%"
+        left="32.16641104294479%" id="af19ffd4-5b6d-408e-b7ba-7c9ed34a3353"
+        v-bind:style="{'width':'','height':'','fontSize':'10.695px','maxWidth':'','minWidth':'','tabIndex':'-0.713','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.89125','marginLeft':'0px','paddingTop':'3.565px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'3.565px','marginBottom':'0px','paddingRight':'3.565px','paddingBottom':'3.565px','textDecoration':'none','zIndex':4}"/>
+      <WBadge href="" color="#FFFFFF" v-bind:exact="false" v-bind:active="false" v-bind:append="false" caption="Badge"
+        variant="secondary" v-bind:disabled="false" linkTarget="_self" tagOfBadge="span" v-bind:pillOfBadge="false"
+        ncTypography="body1" ncTooltipText=""
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'caption','displayName':'Caption'}]}"
+        toForRouterLink="" ncClickFontColor="#0000FF" ncFocusFontColor="#00FF00" ncHoverFontColor="#ff0000"
+        ncDisableFontColor="#4285f4" whichHeadTagForSize="h5" position="absolute" top="41.67022376532421%"
+        left="70.30598159509202%" id="bbd1838c-132a-4f4b-bf63-caba8fbf5f91"
+        v-bind:style="{'width':'','height':'','fontSize':'10.695px','maxWidth':'','minWidth':'','tabIndex':'-0.713','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.89125','marginLeft':'0px','paddingTop':'3.565px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'3.565px','marginBottom':'0px','paddingRight':'3.565px','paddingBottom':'3.565px','textDecoration':'none','zIndex':5}"/>
+      <WPrimaryBadge href="" color="#FFFFFF" v-bind:exact="false" v-bind:active="false" v-bind:append="false"
+        caption="Primary" variant="primary" v-bind:disabled="false" linkTarget="_self" tagOfBadge="span"
+        v-bind:pillOfBadge="false" ncTypography="body1" ncTooltipText=""
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'caption','displayName':'Caption'}]}"
+        toForRouterLink="" ncClickFontColor="#0000FF" ncFocusFontColor="#00FF00" ncHoverFontColor="#ff0000"
+        ncDisableFontColor="#4285f4" whichHeadTagForSize="h5" position="absolute" top="40.337377553650676%"
+        left="57.04754601226994%" id="a52dd4b3-0b38-4a1f-a149-194a63fbde1e"
+        v-bind:style="{'width':'','height':'','fontSize':'10.695px','maxWidth':'','minWidth':'','tabIndex':'-0.713','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.89125','marginLeft':'0px','paddingTop':'3.565px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'3.565px','marginBottom':'0px','paddingRight':'3.565px','paddingBottom':'3.565px','textDecoration':'none','zIndex':6}"/>
+    </NucGridCell>
+  </NucGridPage>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style scoped></style>
